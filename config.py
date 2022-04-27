87b0e178c4d193da5145c82626756df73153638c
Khang <kt36@njit.edu>
2022-04-27 02:39:22 -0400

    add codes

---
+++ config.py
@@ -0,0 +1,98 @@
+import argparse
+from Utils.utils import str2bool
+
+def add_general_group(group):
+    group.add_argument("--gpu", type=int, default=0, help="gpu device ID")
+    group.add_argument("--eval-every", type=int, default=30, help="eval every X selected epochs")
+    group.add_argument("--save-path", type=str, default="results/", help="dir path for output file")
+    group.add_argument("--seed", type=int, default=42, help="seed value")
+    group.add_argument("--num-steps", type=int, default=50000)
+    group.add_argument("--optim", type=str, default='sgd', choices=['adam', 'sgd'], help="learning rate")
+    group.add_argument("--batch-size", type=int, default=100)
+    group.add_argument("--inner-steps", type=int, default=4, help="number of inner steps")
+
+def add_data_group(group):
+    group.add_argument('--dataset', type=str, default='cifar10', help="used dataset")
+    group.add_argument('--num_client', type=int, default=20, help="Number of client in the FL setting")
+    group.add_argument('--num_compromised_client', type=int, default=2,
+                       help="Number of compromised client in the FL setting")
+    group.add_argument('--data_path', type=str, default='data', help="the directory used to save dataset")
+    group.add_argument('--data_verbose', action='store_true', help="print detailed dataset info")
+    group.add_argument('--save_data', action='store_true')
+
+
+
+
+def add_model_group(group):
+    group.add_argument("--n-hidden", type=int, default=3, help="num. hidden layers")
+    group.add_argument("--inner-lr", type=float, default=5e-3, help="learning rate for inner optimizer")
+    group.add_argument("--lr", type=float, default=0.01, help="learning rate")
+    group.add_argument("--wd", type=float, default=1e-3, help="weight decay")
+    group.add_argument("--inner-wd", type=float, default=5e-5, help="inner weight decay")
+    group.add_argument("--embed-dim", type=int, default=-1, help="embedding dim")
+    group.add_argument("--embed-lr", type=float, default=None, help="embedding learning rate")
+    group.add_argument("--hyper-hid", type=int, default=100, help="hypernet hidden dim")
+    group.add_argument("--spec-norm", type=str2bool, default=False, help="hypernet hidden dim")
+    group.add_argument("--nkernels", type=int, default=16, help="number of kernels for cnn model")
+    group.add_argument('--batch_size', type=int, default=100)
+    group.add_argument('--fl_epochs', type=int, default=50)
+    group.add_argument('--train_epochs', type=int, default=10)
+    group.add_argument('--dropout', type=float, default=0.5)
+    group.add_argument('--train_verbose', action='store_true', help="print training details")
+    group.add_argument('--log_every', type=int, default=1, help='print every x epoch')
+    group.add_argument('--eval_every', type=int, default=5, help='evaluate every x epoch')
+    group.add_argument('--clean_model_save_path', type=str, default='../save/model/clean')
+    group.add_argument('--save_clean_model', action='store_true')
+    group.add_argument("--bt", type=int, default=10, help="batch")
+
+
+
+def add_atk_group(group):
+    group.add_argument('--bkd_gratio_train', type=float, default=0.1, help="backdoor graph ratio in trainset")
+    group.add_argument('--bkd_gratio_test', type=float, default=0.5, help="backdoor graph ratio in testset")
+    group.add_argument('--bkd_num_pergraph', type=int, default=1, help="number of backdoor triggers per graph")
+    group.add_argument('--bkd_size', type=int, default=5, help="number of nodes for each trigger")
+    group.add_argument('--target_class', type=int, default=None, help="the targeted node/graph label")
+
+    group.add_argument('--gtn_layernum', type=int, default=3, help="layer number of GraphTrojanNet")
+    group.add_argument('--pn_rate', type=float, default=1,
+                       help="ratio between trigger-embedded graphs (positive) and benign ones (negative)")
+    group.add_argument('--gtn_input_type', type=str, default='2hop',
+                       help="how to process org graphs before inputting to GTN")
+
+    group.add_argument('--resample_steps', type=int, default=3, help="# iterations to re-select graph samples")
+    group.add_argument('--bilevel_steps', type=int, default=4, help="# bi-level optimization iterations")
+    group.add_argument('--gtn_lr', type=float, default=0.01)
+    group.add_argument('--rlr_thres', type=float, default=0.01)
+    group.add_argument('--gtn_epochs', type=int, default=20, help="# attack epochs")
+    group.add_argument('--topo_activation', type=str, default='sigmoid',
+                       help="activation function for topology generator")
+    group.add_argument('--feat_activation', type=str, default='relu', help="activation function for feature generator")
+    group.add_argument('--topo_thrd', type=float, default=0.5, help="threshold for topology generator")
+    group.add_argument('--feat_thrd', type=float, default=0,
+                       help="threshold for feature generator (only useful for binary feature)")
+
+    group.add_argument('--lambd', type=float, default=1, help="a hyperparameter to balance attack loss components")
+    # group.add_argument('--atk_verbose', action='store_true', help="print attack details")
+    group.add_argument('--save_bkd_model', action='store_true')
+    group.add_argument('--bkd_model_save_path', type=str, default='../save/model/bkd')
+
+
+def add_defense_group(group):
+    group.add_argument('--grad_clip', type=float, default=0.1, help="clipping bound for user-dp")
+    group.add_argument('--noise_scale', type=float, default=0.1, help="noise scale for user-dp")
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    data_group = parser.add_argument_group(title="Data-related configuration")
+    model_group = parser.add_argument_group(title="Model-related configuration")
+    atk_group = parser.add_argument_group(title="Attack-related configuration")
+    general_group = parser.add_argument_group(title="General configuration")
+    defense_group = parser.add_argument_group(title="Defense configuration")
+
+    add_data_group(data_group)
+    add_model_group(model_group)
+    add_atk_group(atk_group)
+    add_general_group(general_group)
+    add_defense_group(defense_group)
+    return parser.parse_args()
---
+++ main/userdp_hypernet_cifar10.py
@@ -0,0 +1,243 @@
+import json
+import logging
+import random
+from collections import OrderedDict, defaultdict
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import torch
+import torch.utils.data
+from tqdm import trange
+import time
+from Models.models import CNNHyper, CNNTarget
+from Data.node_noniid import BaseNodes
+from config import parse_args
+from Utils.utils import get_device, set_logger, set_seed, get_gaussian_noise
+from copy import deepcopy
+
+
+def eval_model(nodes, num_nodes, hnet, net, criteria, device, split):
+    curr_results = evaluate(nodes, num_nodes, hnet, net, criteria, device, split=split)
+    total_correct = sum([val['correct'] for val in curr_results.values()])
+    total_samples = sum([val['total'] for val in curr_results.values()])
+    avg_loss = np.mean([val['loss'] for val in curr_results.values()])
+    avg_acc = total_correct / total_samples
+
+    all_acc = [val['correct'] / val['total'] for val in curr_results.values()]
+
+    return curr_results, avg_loss, avg_acc, all_acc
+
+
+@torch.no_grad()
+def evaluate(nodes: BaseNodes, num_nodes, hnet, net, criteria, device, split='test'):
+    hnet.eval()
+    results = defaultdict(lambda: defaultdict(list))
+
+    for node_id in range(num_nodes):  # iterating over nodes
+
+        running_loss, running_correct, running_samples = 0., 0., 0.
+        if split == 'test':
+            curr_data = nodes.test_loaders[node_id]
+        elif split == 'val':
+            curr_data = nodes.val_loaders[node_id]
+        else:
+            curr_data = nodes.train_loaders[node_id]
+
+        for batch_count, batch in enumerate(curr_data):
+            img, label = tuple(t.to(device) for t in batch)
+
+            weights, _ = hnet(torch.tensor([node_id], dtype=torch.long).to(device))
+            net.load_state_dict(weights)
+            pred = net(img)
+            running_loss += criteria(pred, label).item()
+            running_correct += pred.argmax(1).eq(label).sum().item()
+            running_samples += len(label)
+
+        results[node_id]['loss'] = running_loss / (batch_count + 1)
+        results[node_id]['correct'] = running_correct
+        results[node_id]['total'] = running_samples
+
+    return results
+
+
+def train(args, device) -> None:
+    # training params
+    nodes = BaseNodes(args.data_name, args.data_path, args.num_nodes, classes_per_node=args.classes_per_node,
+                      batch_size=args.batch_size)
+    sampling_prob = args.bt / args.num_nodes
+    embed_dim = args.embed_dim
+
+    if embed_dim == -1:
+        logging.info("auto embedding size")
+        embed_dim = int(1 + args.num_nodes / 4)
+
+    if args.data_name == "cifar10":
+        hnet = CNNHyper(args.num_nodes, embed_dim, hidden_dim=args.hyper_hid, n_hidden=args.n_hidden,
+                        n_kernels=args.nkernels)
+        net = CNNTarget(n_kernels=args.nkernels)
+    elif args.data_name == "cifar100":
+        hnet = CNNHyper(args.num_nodes, embed_dim, hidden_dim=args.hyper_hid,
+                        n_hidden=args.n_hidden, n_kernels=args.nkernels, out_dim=100)
+        net = CNNTarget(n_kernels=args.nkernels, out_dim=100)
+    else:
+        raise ValueError("choose data_name from ['cifar10', 'cifar100']")
+
+    hnet = hnet.to(device)
+    net = net.to(device)
+
+    ##################
+    # init optimizer #
+    ##################
+    embed_lr = args.embed_lr if args.embed_lr is not None else args.lr
+    optimizers = {
+        'sgd': torch.optim.SGD(
+            [
+                {'params': [p for n, p in hnet.named_parameters() if 'embed' not in n]},
+                {'params': [p for n, p in hnet.named_parameters() if 'embed' in n], 'lr': embed_lr}
+            ], lr=args.lr  # , momentum=0.9, weight_decay=wd
+        ),
+        'adam': torch.optim.Adam(params=hnet.parameters(), lr=args.lr)
+    }
+    optimizer = optimizers[args.optim]
+    criteria = torch.nn.CrossEntropyLoss()
+
+    ################
+    # init metrics #
+    ################
+    last_eval = -1
+    best_step = -1
+    best_acc = -1
+    test_best_based_on_step, test_best_min_based_on_step = -1, -1
+    test_best_max_based_on_step, test_best_std_based_on_step = -1, -1
+    step_iter = trange(args.num_steps)
+    print('steps', args.num_steps)
+    print('step_iter', step_iter)
+    print('save_path', args.save_path)
+
+    results = defaultdict(list)
+
+    name_add = 'train_batch_n' + str(args.num_nodes) + '_nc' + str(args.bt) + '_lr' + str(args.lr) + '_ilr' + str(
+        args.inner_lr) + '_seed' + str(args.seed) + '_noniid_c2'
+
+    step_vect = []
+    for step in step_iter:
+        start_time = time.time()
+        hnet.train()
+
+        node_id_vect = random.sample(range(args.num_nodes), args.bt)
+
+        hnet_grads_all = defaultdict(list)
+        c = 0
+        for node_id in node_id_vect:
+            # produce & load local network weights
+            weights, _ = hnet(torch.tensor([node_id], dtype=torch.long).to(device))
+            net.load_state_dict(weights)
+
+            # init inner optimizer
+            inner_optim = torch.optim.SGD(
+                net.parameters(), lr=args.inner_lr, momentum=.9, weight_decay=args.inner_wd
+            )
+
+            # storing theta_i for later calculating delta theta
+            inner_state = OrderedDict({k: tensor.data for k, tensor in weights.items()})
+
+            # inner updates -> obtaining theta_tilda
+            for i in range(args.inner_steps):
+                net.train()
+                inner_optim.zero_grad()
+                optimizer.zero_grad()
+
+                batch = next(iter(nodes.train_loaders[node_id]))
+                img, label = tuple(t.to(device) for t in batch)
+
+                pred = net(img)
+
+                loss = criteria(pred, label)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(net.parameters(), 50)
+
+                inner_optim.step()
+
+            optimizer.zero_grad()
+
+            final_state = net.state_dict()
+            delta_theta = OrderedDict({k: inner_state[k] - final_state[k] for k in weights.keys()})
+
+            hnet_grads_each = torch.autograd.grad(
+                list(weights.values()), hnet.parameters(), grad_outputs=list(delta_theta.values())
+            )
+            # hnet.embeddings.weight.grad = hnet_grads_each[0]
+            # optimizer.step()
+
+            if c == 0:
+                hnet_grads = deepcopy(list(hnet_grads_each))
+                for t in range(len(hnet_grads)):
+                    hnet_grads[t] = hnet_grads[t] / args.bt
+            else:
+                tmp = list(hnet_grads_each)
+                for t in range(len(hnet_grads)):
+                    hnet_grads[t] += tmp[t] / args.bt
+            c += 1
+
+        for p, g in zip(hnet.parameters(), hnet_grads):
+            p.grad = g
+
+        # for n, p in hnet.named_parameters():
+        #     if 'embed' in n:
+        #         p.grad = None
+
+        torch.nn.utils.clip_grad_norm_(hnet.parameters(), args.grad_clip)
+        for p in hnet.parameters():
+            p.grad = p.grad + torch.normal(0, get_gaussian_noise(clipping_noise=args.grad_clip,
+                                                                 noise_scale=args.noise_scale,
+                                                                 sampling_prob=sampling_prob,
+                                                                 num_client=args.num_comp_cli),p.grad.size())
+        optimizer.step()
+
+        if step % 10 == 0:
+            last_eval = step
+            step_results, avg_loss, avg_acc, all_acc = eval_model(nodes, args.num_nodes, hnet, net, criteria, device,
+                                                                  split="test")
+            logging.info(f"\nStep: {step + 1}, AVG Loss: {avg_loss:.4f},  AVG Acc: {avg_acc:.4f}")
+
+            results['test_avg_loss'].append(avg_loss)
+            results['test_avg_acc'].append(avg_acc)
+
+            _, val_avg_loss, val_avg_acc, _ = eval_model(nodes, args.num_nodes, hnet, net, criteria, device,
+                                                         split="val")
+            if best_acc < val_avg_acc:
+                best_acc = val_avg_acc
+                best_step = step
+                test_best_based_on_step = avg_acc
+                test_best_min_based_on_step = np.min(all_acc)
+                test_best_max_based_on_step = np.max(all_acc)
+                test_best_std_based_on_step = np.std(all_acc)
+
+            results['val_avg_loss'].append(val_avg_loss)
+            results['val_avg_acc'].append(val_avg_acc)
+            results['best_step'].append(best_step)
+            results['best_val_acc'].append(best_acc)
+            my_csv = pd.DataFrame(results)
+            name_save = args.save_path + name_add + '.csv'
+            my_csv.to_csv(name_save, index=False)
+            with open(args.save_path + name_add + '.pt', 'wb') as f:  # bd0.5_cr0_double bd0.1_cr2
+                torch.save([hnet, net], f)
+
+        print('Finish one step in ', time.time() - start_time)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    assert args.gpu <= torch.cuda.device_count(), f"--gpu flag should be in range [0,{torch.cuda.device_count() - 1}]"
+
+    set_logger()
+    set_seed(args.seed)
+
+    device = get_device(gpus=args.gpu)
+
+    if args.data_name == 'cifar10':
+        args.classes_per_node = 2
+    else:
+        args.classes_per_node = 10
+
+    train(args, device)
