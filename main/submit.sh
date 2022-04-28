0ccdd9aef0ae3a83d8907a8e0a505b0e9253a3ce
khangtran2020 <kt36@njit.edu>
2022-04-28 14:41:21 -0400

    add submit file for lochness

---
+++ main/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+# The above line must always be first, and must have "-l"
+#SBATCH -J hpn_udp
+#SBATCH -p datasci
+#SBATCH --output= ../results/userdp_hypernet.out
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:1
+module load python
+conda activate hypernet
+python userdp_hypernet_cifar10.py --grad_clip 1.0 --delta 0.00001 --noise_scale 1.5 --num_comp_cli 1
+
