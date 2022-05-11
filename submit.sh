4157720f62d0c3bd65d08208044415c576c5c08f
khangtran2020 <kt36@njit.edu>
2022-05-10 23:57:43 -0400

    update submit

--- submit.sh
+++ submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 # The above line must always be first, and must have "-l"
-#SBATCH -J hpn_udp
+#SBATCH -J hpn_clean
 #SBATCH -p datasci
-#SBATCH --output= results/clean_bt_80_1000_step.out
+#SBATCH --output=results/hpn_clean_bt_80_step_1000.out
 #SBATCH --mem=48G
 #SBATCH --gres=gpu:1
 module load python
 conda activate hypernet
-python userdp_hypernet_cifar10.py --train_mode clean --grad_clip 1.0 --delta 0.00001 --noise_scale 1.5 --num_comp_cli 1 --bt 80 --num_steps 1000
+python main.py --train_mode clean --grad_clip 1.0 --delta 0.00001 --noise_scale 1.5 --num_comp_cli 1 --bt 80 --num_steps 1000
 
