6c375f66a51f9cca303aa9bebb43991e1a889f4a
Khang <kt36@njit.edu>
2022-05-18 00:34:00 -0400

    updating code

--- Data/dataset_noniid.py
+++ Data/dataset_noniid.py
@@ -22,8 +22,7 @@ class EmbeddingTransform:
     def __call__(self, x):
         embedding = self.model(x)
         size = embedding.size().item()
-        print(size)
-        return embedding
+        return embedding.view(size[0], size[-1]*size[-2])
 
 
 def get_datasets(data_name, dataroot, normalize=True, bd_size=10000, use_embeddings=False):  # 10000
