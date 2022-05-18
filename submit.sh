0690278cf9d374910e22713ea32a9d0db2918a88
Khang <kt36@njit.edu>
2022-05-18 10:44:17 -0400

    updating code

--- submit.sh
+++ submit.sh
@@ -7,5 +7,10 @@
 #SBATCH --gres=gpu:1
 module load python
 conda activate hypernet
-python main.py --train_mode userdp --grad_clip 0.000001 --udp_delta 0.00001 --noise_scale 50  --num_comp_cli 1 --bt 40 --num_steps 10000 --optim adam --lr 0.0001 --num_draws_udp 1000
+python main.py --train_mode userdp --grad_clip 0.000001 --udp_delta 0.00001 \
+--bt 40 \
+--num_steps 10000 \
+--noise_scale 50 \
+--optim adam \
+--lr 0.0001
 
